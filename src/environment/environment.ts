7024b81cef9011691ca48fbbe9717fb67667ff67
leozhao0709 <zhao434@usc.edu>
2018-04-02 22:34:34 -0700

    production

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -8,14 +8,12 @@ export let environment: {
     GOOGLE_CALL_BACK_URL: string;
 } = require('../config/env.json');
 
-if (process.env.NODE_ENV === 'prod') {
+if (process.env.NODE_ENV === 'production') {
     const prodEnv = require('../config/env.prod.json');
     environment = {
         ...environment,
         ...prodEnv
     };
-    // tslint:disable-next-line:no-console
-    console.log(environment);
 } else if (process.env.NODE_ENV === 'test') {
     const testEnv = require('../config/env.test.json');
     environment = {
