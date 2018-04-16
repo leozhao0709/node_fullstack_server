ab441eaaf9e9b205b638149b23b29bf11df091dd
leozhao0709 <zhao434@usc.edu>
2018-04-15 20:45:00 -0700

    update route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,48 @@
 import * as React from 'react';
-import { Route, BrowserRouter } from 'react-router-dom';
+import { Switch, Route, BrowserRouter, Redirect } from 'react-router-dom';
 import Layout from './hoc/Layout/Layout';
 import Landing from './containers/Landing/Landing';
 import Dashboard from './containers/Dashboard/Dashboard';
 import Survey from './containers/Survey/Surver';
+import { StoreState } from './store/store';
+import { connect } from 'react-redux';
 
 interface AppDispatchProps {
 
 }
 
 interface AppStateProps {
+  user?: { _id: string, email: string, credits: number } | null;
 }
 
 class App extends React.Component<AppDispatchProps & AppStateProps, {}> {
 
   render() {
+    let homeRoute = <Route path="/" exact componenet={Landing} />;
+
+    if (this.props.user) {
+      homeRoute = <Redirect from="/" to="/dashboard" />;
+    }
+
     return (
       <BrowserRouter>
         <Layout>
-          {/* <Switch> */}
-          <Route path="/surveys" exact component={Survey} />
-          <Route path="/dashboard" exact component={Dashboard} />
-          <Route path="/" exact component={Landing} />
-          {/* <Route render={() => <h1>Not found</h1>} /> */}
-          {/* </Switch> */}
+          <Switch>
+            <Route path="/surveys" exact component={Survey} />
+            <Route path="/dashboard" exact component={Dashboard} />
+            {homeRoute}
+            <Route render={() => <h1>Not found</h1>} />
+          </Switch>
         </Layout>
       </BrowserRouter>
     );
   }
 }
 
-export default App;
+const mapStateProps = (storeState: StoreState): AppStateProps => {
+  return {
+    user: storeState.auth.user
+  };
+};
+
+export default connect(mapStateProps)(App);
